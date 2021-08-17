aa637d229480befa49e27bd6392ccbd5cecf02e8
Anna Shramova <anna.nexus.2002@gmail.com>
2021-08-17 16:06:52 +0300

    Update contracts

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -13,10 +13,19 @@ contract Escrow is Accessible {
   address public seller;
   address public mediator;
 
+  ///@notice ERC20 contract
   IERC20Upgradeable public token;
-  IERC721Upgradeable public NFTtoken;
+  ///@notice ERC721 contract
+  IERC721Upgradeable public tokenNFT;
+
+  // ///@dev stakeholder -> stake
+  // mapping(address => uint256) internal stakes;
+
+  // ///@dev stakeNFTholder -> tokenId
+  // mapping(address => uint256) internal stakeNFT;
 
   enum State {
+    None,
     AwaitingPayment,
     AwaitingDelivery,
     Complete
@@ -26,9 +35,13 @@ contract Escrow is Accessible {
     address buyer;
     address seller;
     address mediator;
+    uint256 amount;
+    uint256 tokenId;
     State tradeState;
+    bool existence;
   }
 
+  Trade[] public trades;
 
   modifier onlyMediator(uint256 _tradeIndex) {
     require(msg.sender == trades[_tradeIndex].mediator);
@@ -45,29 +58,236 @@ contract Escrow is Accessible {
     _;
   }
 
+  modifier tradeExist(uint256 _tradeIndex) {
+    require(trades[tradeIndex].existence, "Trade doesn't exist");
+    _;
+  }
+
+  modifier awaitingPayment(uint256 _tradeIndex) {
+    require(
+      trades[tradeIndex].tradeState = State.AwaitingPayment,
+      "Wrong state for execution "
+    );
+    _;
+  }
+  modifier awaitingDelivery(uint256 _tradeIndex) {
+    require(
+      trades[tradeIndex].tradeState = State.AwaitingDelivery,
+      "Wrong state for execution "
+    );
+    _;
+  }
+  modifier notZeroAddress(
+    address _seller,
+    address _buyer,
+    address _mediator
+  ) {
+    require(seller == address(0), "Seller is zero address");
+    require(buyer == address(0), "Buyer is zero address");
+    require(mediator == address(0), "Mediator is zero address");
+    _;
+  }
+
+  event StakedNFT(
+    address _seller,
+    uint256 indexed _tokenId,
+    uint256 indexed _tradeIndex
+  );
+
+  event Staked(
+    address _buyer,
+    uint256 indexed _amount,
+    uint256 indexed _tradeIndex
+  );
+
+  event Confirmed(
+    address _mediator,
+    uint256 indexed _amount,
+    uint256 indexed _tradeIndex
+  );
+
+  event TradeCreated(
+    address _mediator,
+    address _buyer,
+    address _seller,
+    uint256 indexed _amount,
+    uint256 indexed _tokenId,
+    uint256 indexed _tradeIndex
+  );
+
+  event Unstaked(
+    address _buyer,
+    uint256 indexed _amount,
+    uint256 indexed _tradeIndex
+  );
+
+  event UnstakedNFT(
+    address _seller,
+    uint256 indexed _tokenId,
+    uint256 indexed _tradeIndex
+  );
+
+  event RevokeConfirm(
+    address _mediator,
+    address _buyer,
+    address _seller,
+    uint256 indexed _amount,
+    uint256 indexed _tokenId,
+    uint256 indexed _tradeIndex
+  );
+
   function initialize(
     address _owner,
     address _buyer,
     address _seller,
     address _mediator,
     IERC20Upgradeable _token,
-    IERC721Upgradeable _NFTtoken
-  ) public initializer {
-    require(_owner != address(0));
-    buyer = _buyer;
+    IERC721Upgradeable _tokenNFT
+  ) public initializer notZeroAddress(_seller, _buyer, _mediator) {
     seller = _seller;
+    buyer = _buyer;
     mediator = _mediator;
 
     token = _token;
-    NFTtoken = _NFTtoken;
+    tokenNFT = _tokenNFT;
 
     super.initialize(_owner);
   }
-  function createTrade() external {}
 
-  function stakeNFT() external {}
+  function createTrade(
+    address _seller,
+    address _mediator,
+    address _buyer,
+    uint256 _amount,
+    uint256 _tokenId
+  ) external notZeroAddress(_seller, _buyer, _mediator) {
+    uint256 tradeIndex = trades.length;
+    require(!trades[tradeIndex].existance, "Trade already exists");
+    trades.push(
+      Trade({
+        buyer: _buyer,
+        seller: _seller,
+        mediator: _mediator,
+        amount: _amount,
+        tokenId: _tokenId,
+        tradeState: State.None,
+        existance: true
+      })
+    );
 
-  function stake() external {}
+    emit TradeCreated(
+      _mediator,
+      _buyer,
+      _seller,
+      _amount,
+      _tokenId,
+      _tradeIndex
+    );
+  }
 
-  function confirmDelivery() external {}
+  function stakeNFT(uint256 _tradeIndex)
+    external
+    tradeExist(_tradeIndex)
+    onlySeller(_tradeIndex)
+  {
+    require(
+      trades[tradeIndex].tradeState == State.None,
+      "Wrong status for staking NFT"
+    );
+
+    tokenNFT.safeTransferFrom(
+      msg.sender,
+      address(this),
+      trades[tradeIndex].tokenId
+    );
+    trades[tradeIndex].tradeState = State.AwaitingPayment;
+
+    emit StakedNFT(_seller, _tokenId, _tradeIndex);
+  }
+
+  function stake(uint256 _tradeIndex)
+    external
+    tradeExist(_tradeIndex)
+    onlyBuyer(_tradeIndex)
+    awatingPayment(_tradeIndex)
+  {
+    token.safeTransferFrom(
+      msg.sender,
+      address(this),
+      trades[tradeIndex].amount
+    );
+    trades[tradeIndex].tradeState = State.AwaitingDelivery;
+    emit Staked(_buyer, _amount, _tradeIndex);
+  }
+
+  function confirmDelivery(uint256 _tradeIndex)
+    external
+    tradeExist(_tradeIndex)
+    onlyMediator(_tradeIndex)
+    awaitingDelivery(_tradeIndex)
+  {
+    token.safeTransferFrom(
+      trades[_tradeIndex].buyer,
+      trades[_tradeIndex].seller,
+      trades[tradeIndex].amount
+    );
+    tokenNFT.safeTransferFrom(
+      trades[_tradeIndex].seller,
+      trades[_tradeIndex].buyer,
+      trades[tradeIndex].tokenId
+    );
+    trades[tradeIndex].tradeState = State.Complete;
+    emit Confirmed(_mediator, _amount, _tradeIndex);
+  }
+
+  function unstake(uint256 _tradeIndex)
+    external
+    onlyBuyer(_tradeIndex)
+    tradeExist(_tradeIndex)
+    awaitingDelivery(_tradeIndex)
+  {
+    uint256 amount = trades[_tradeIndex].amount;
+    uint256 tokenId = trades[_tradeIndex].tokenId;
+    address seller = trades[_tradeIndex].seller;
+
+    stakes[msg.sender] = stakes[msg.sender].sub(amount);
+
+    token.transfer(msg.sender, amount);
+    tokenNFT.transfer(seller, tokenId);
+
+    emit Unstaked(msg.sender, amount, _tradeIndex);
+  }
+
+  function unstakeNFT(uint256 _tradeIndex)
+    external
+    onlySeller(_tradeIndex)
+    tradeExist(_tradeIndex)
+    awaitingPayment(_tradeIndex)
+  {
+    uint256 tokenId = trades[_tradeIndex].tokenId;
+    tokenNFT.transfer(msg.sender, tokenId);
+
+    emit UnstakedNFT(msg.sender, tokenId, _tradeIndex);
+  }
+
+  function revokeDelivery(uint256 _tradeIndex)
+    external
+    onlyMediator(_tradeIndex)
+    tradeExist(_tradeIndex)
+    awaitingDelivery(_tradeIndex)
+  {
+    uint256 amount = trades[_tradeIndex].amount;
+    uint256 tokenId = trades[_tradeIndex].tokenId;
+    address seller = trades[_tradeIndex].seller;
+    address buyer = trades[_tradeIndex].buyer;
+
+    token.transfer(buyer, amount);
+    tokenNFT.transfer(seller, tokenId);
+
+    emit RevokeConfirm(msg.sender, buyer, seller, amount, tokenId, _tradeIndex);
+  }
+
+  function getTradeState(uint256 _tradeIndex) external view {
+    return trades[_tradeIndex].tradeState;
+  }
 }
