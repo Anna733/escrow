105fa020ac0be26d44985279ea11da7c70649534
Anna Shramova <anna.nexus.2002@gmail.com>
2021-08-26 21:08:02 +0300

    Add ERC20 and ERC721 stub

--- contracts/test/ERC20Stub.sol
+++ contracts/test/ERC20Stub.sol
@@ -4,8 +4,11 @@ pragma solidity 0.8.6;
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
 contract ERC20Stub is ERC20Upgradeable {
-  function initialize(uint256 initialSupply) public virtual initializer {
+  function initialize() public virtual initializer {
     __ERC20_init("Test", "TST");
-    _mint(msg.sender, initialSupply);
+  }
+
+  function mint(address _to, uint256 _amount) external {
+    _mint(_to, _amount);
   }
 }
--- contracts/test/ERC721Stub.sol
+++ contracts/test/ERC721Stub.sol
@@ -4,8 +4,11 @@ pragma solidity 0.8.6;
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 
 contract ERC721Stub is ERC721Upgradeable {
-  function initialize(uint256 tokenId) public virtual initializer {
+  function initialize() public virtual initializer {
     __ERC721_init("Test", "TST");
-    _mint(msg.sender, tokenId);
+  }
+
+  function mint(address _to, uint256 _tokenId) external {
+    _mint(_to, _tokenId);
   }
 }
